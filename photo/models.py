3d9cbc42efef6c1b014425353103338ba3316e4a
kco4776 <kco4776@naver.com>
2020-07-29 10:53:33 +0900

    create photo model

--- photo/models.py
+++ photo/models.py
@@ -4,4 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Photo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    text = models.TextField(blank=True)
+    image = models.ImageField(upload_to='timeline_photo/%y/%m/%d')
+    created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return "text : " + self.text
