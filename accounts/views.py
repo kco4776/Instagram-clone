63fd2e07af6703b1ba0bcb4a7d49417974ca259c
kco4776 <kco4776@naver.com>
2020-08-05 11:20:44 +0900

    signup 기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        print(username, password, password2)
+
+        user = User()
+        user.username = username
+        user.set_password(password)
+        user.save()
+        return render(request, 'accounts/signup_complete.html')
+    else:
+        context_values = {'form': 'this is form'}
+        return render(request, 'accounts/signup.html', context_values)
